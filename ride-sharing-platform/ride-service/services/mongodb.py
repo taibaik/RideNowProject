6673d2085def26771e21af7ee5b0e4171d889e36
Haiqal <101733400+taibaik@users.noreply.github.com>
2025-06-16 20:19:47 +0700

    remove nested ride-sharing-platform folder

--- ride-sharing-platform/ride-service/services/mongodb.py
+++
@@ -1,8 +0,0 @@
-# services/mongodb.py
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongodb:27017")
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["ride_sharing"]
-ride_collection = db["rides"]
